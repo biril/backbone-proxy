108f6dbfb3d8ac6676bff1cb85e282a539c86b51
biril <alexlambiris@gmail.com>
2014-09-01 11:47:53 +0200

    Revise test-set() - add tests for has()

--- test/test-set().js
+++ test/test-set().js
@@ -6,55 +6,61 @@
     setup: function () {},
   });
 
+  var
+
+    proxied, proxy, proxy2, proxyProxy, models, targetModel, otherModel,
+
+    setup = function (tM, oM) {
+      var Proxy, ProxyProxy;
+
+      proxied    = new Backbone.Model({ name: 'Anna', age: 23 });
+      Proxy      = BackboneProxy.extend(proxied);
+
+      proxy      = new Proxy();
+      proxy2     = new Proxy();
+      ProxyProxy = BackboneProxy.extend(proxy);
+
+      proxyProxy = new ProxyProxy();
+
+      models = {
+        proxied: proxied,
+        proxy: proxy,
+        proxy2: proxy2,
+        proxyProxy: proxyProxy
+      };
+
+      tM && (targetModel = models[tM]);
+      oM && (otherModel  = models[oM]);
+    },
+
+    s = function (tM, oM, expect) {
+      return function () {
+        setup(tM, oM);
+        expect();
+      };
+    };
+
   _(['proxied', 'proxy', 'proxy2', 'proxyProxy']).each(function (tM) { // target model
 
     _(['proxied', 'proxy', 'proxy2', 'proxyProxy']).each(function (oM) { // other model
 
-      var
-
-        proxied, proxy, proxy2, proxyProxy, models, targetModel, otherModel,
-
-        setup = function () {
-          var Proxy, ProxyProxy;
-
-          proxied    = new Backbone.Model({ name: 'Anna', age: 23 });
-          Proxy      = BackboneProxy.extend(proxied);
-
-          proxy      = new Proxy();
-          proxy2     = new Proxy();
-          ProxyProxy = BackboneProxy.extend(proxy);
-
-          proxyProxy = new ProxyProxy();
-
-          models = {
-            proxied: proxied,
-            proxy: proxy,
-            proxy2: proxy2,
-            proxyProxy: proxyProxy
-          };
-
-          targetModel = models[tM];
-          otherModel  = models[oM];
-        },
-
-        s = function (expect) {
-          return function () {
-            setup();
-            expect();
-          };
-        };
-
-      test(tM + '.set() should set attribute\'s value on ' + oM, 1, s(function () {
+      test(tM + '.set() should set attribute\'s value on ' + oM, 1, s(tM, oM, function () {
         targetModel.set({ name: 'Betty' });
         strictEqual(otherModel.get('name'), 'Betty');
       }));
 
-      test(tM + '.unset() should unset attribute\'s value on ' + oM, 1, s(function () {
+      test(tM + '.set() should set new attributes on ' + oM, 2, s(tM, oM, function () {
+        targetModel.set({ surname: 'Freud' });
+        strictEqual(otherModel.has('surname'), true, oM + ' has(new attr)');
+        strictEqual(otherModel.get('surname'), 'Freud', oM + ' new attr is of expected value');
+      }));
+
+      test(tM + '.unset() should unset attribute\'s value on ' + oM, 1, s(tM, oM, function () {
         targetModel.unset('name');
         strictEqual(otherModel.get('name'), undefined);
       }));
 
-      test(tM + '.clear() should unset all attributes on ' + oM, 2, s(function () {
+      test(tM + '.clear() should unset all attributes on ' + oM, 2, s(tM, oM, function () {
         targetModel.clear();
         strictEqual(otherModel.get('name'), undefined);
         strictEqual(otherModel.get('age'), undefined);
