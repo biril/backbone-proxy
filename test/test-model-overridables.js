25e9eb580c5cbf5d94826b47e22e7b5c9bdfff13
biril <alexlambiris@gmail.com>
2014-10-09 17:55:35 +0200

    Fix indentation on overridables-related tests
    and also get rid of rogue comma.

--- test/test-model-overridables.js
+++ test/test-model-overridables.js
@@ -1,96 +1,96 @@
 /*global Backbone, BackboneProxy, QUnit, test, strictEqual, ok */
 (function () {
-    'use strict';
+  'use strict';
 
-    var proxied, proxy;
+  var proxied, proxy;
 
-    QUnit.module('model overridables', {
-        setup: function () {
-            var Proxy;
-            proxied = new Backbone.Model({ name: 'Anna' });
-            Proxy = BackboneProxy.extend(proxied);
-            proxy = new Proxy();
-        },
-    });
+  QUnit.module('model overridables', {
+    setup: function () {
+      var Proxy;
+      proxied = new Backbone.Model({ name: 'Anna' });
+      Proxy = BackboneProxy.extend(proxied);
+      proxy = new Proxy();
+    }
+  });
 
-    //////// .sync() method
+  //////// .sync() method
 
-    test('setting a .sync() method on proxy should have no effect', 0, function () {
-        proxy.sync = function () {
-            ok(false, 'the sync method should not be invoked');
-        };
-    });
+  test('setting a .sync() method on proxy should have no effect', 0, function () {
+    proxy.sync = function () {
+      ok(false, 'the sync method should not be invoked');
+    };
+  });
 
-    test('a .sync() method set on proxied should be invoked when proxy is persisted', 4, function () {
-        proxied.sync = function (method) {
-            strictEqual(method, 'read', 'invoked for fetch');
-        };
-        proxy.fetch();
+  test('a .sync() method set on proxied should be invoked when proxy is persisted', 4, function () {
+    proxied.sync = function (method) {
+      strictEqual(method, 'read', 'invoked for fetch');
+    };
+    proxy.fetch();
 
-        proxied.sync = function (method) {
-            strictEqual(method, 'create', 'invoked for save (when model.isNew())');
-        };
-        proxy.save();
+    proxied.sync = function (method) {
+      strictEqual(method, 'create', 'invoked for save (when model.isNew())');
+    };
+    proxy.save();
 
-        proxied.sync = function (method) {
-            strictEqual(method, 'update', 'invoked for save (when !model.isNew())');
-        };
-        proxy.save({ id: 1 });
+    proxied.sync = function (method) {
+      strictEqual(method, 'update', 'invoked for save (when !model.isNew())');
+    };
+    proxy.save({ id: 1 });
 
-        proxied.sync = function (method) {
-            strictEqual(method, 'delete', 'invoked for destroy (when !model.isNew())');
-        };
-        proxy.set({ id: 1 });
-        proxy.destroy();
-    });
+    proxied.sync = function (method) {
+      strictEqual(method, 'delete', 'invoked for destroy (when !model.isNew())');
+    };
+    proxy.set({ id: 1 });
+    proxy.destroy();
+  });
 
-    test('a .sync() method set on proxied should be invoked with proxied as the model parameter when proxy is persisted', 4, function () {
-        proxied.sync = function (method, model) {
-            strictEqual(model, proxied, 'ok for fetch');
-        };
-        proxy.fetch();
+  test('a .sync() method set on proxied should be invoked with proxied as the model parameter when proxy is persisted', 4, function () {
+    proxied.sync = function (method, model) {
+      strictEqual(model, proxied, 'ok for fetch');
+    };
+    proxy.fetch();
 
-        proxied.sync = function (method, model) {
-            strictEqual(model, proxied, 'ok for save (when model.isNew())');
-        };
-        proxy.save();
+    proxied.sync = function (method, model) {
+      strictEqual(model, proxied, 'ok for save (when model.isNew())');
+    };
+    proxy.save();
 
-        proxied.sync = function (method, model) {
-            strictEqual(model, proxied, 'ok for save (when !model.isNew())');
-        };
-        proxy.save({ id: 1 });
+    proxied.sync = function (method, model) {
+      strictEqual(model, proxied, 'ok for save (when !model.isNew())');
+    };
+    proxy.save({ id: 1 });
 
-        proxied.sync = function (method, model) {
-            strictEqual(model, proxied, 'ok for destroy (when !model.isNew())');
-        };
-        proxy.set({ id: 1 });
-        proxy.destroy();
-    });
+    proxied.sync = function (method, model) {
+      strictEqual(model, proxied, 'ok for destroy (when !model.isNew())');
+    };
+    proxy.set({ id: 1 });
+    proxy.destroy();
+  });
 
-    //////// .idAttribute property
+  //////// .idAttribute property
 
-    test('setting idAttribute on proxy should have no effect', 2, function () {
-        proxy.set({ id: '01234'});
-        proxy.idAttribute = '_id';
+  test('setting idAttribute on proxy should have no effect', 2, function () {
+    proxy.set({ id: '01234'});
+    proxy.idAttribute = '_id';
 
-        ok(!proxied.isNew(), 'proxied is not new, although idAttribute is changed');
-        ok(!proxy.isNew(), 'proxy is not new, although idAttribute is changed');
-    });
+    ok(!proxied.isNew(), 'proxied is not new, although idAttribute is changed');
+    ok(!proxy.isNew(), 'proxy is not new, although idAttribute is changed');
+  });
 
-    test('setting idAttribute on proxied should have the intended effect', 2, function () {
-        proxy.set({ id: '01234'});
-        proxied.idAttribute = '_id';
+  test('setting idAttribute on proxied should have the intended effect', 2, function () {
+    proxy.set({ id: '01234'});
+    proxied.idAttribute = '_id';
 
-        ok(proxied.isNew(), 'proxied with changed idAttribute is new');
-        ok(proxy.isNew(), 'proxy with changed idAttribute is new');
-    });
+    ok(proxied.isNew(), 'proxied with changed idAttribute is new');
+    ok(proxy.isNew(), 'proxy with changed idAttribute is new');
+  });
 
-    // TODO:
+  // TODO:
 
-    //////// .toJSON() method
-    //////// .validate() method
-    //////// .url() method
-    //////// .urlRoot() method
-    //////// .parse() method
+  //////// .toJSON() method
+  //////// .validate() method
+  //////// .url() method
+  //////// .urlRoot() method
+  //////// .parse() method
 
 }());
